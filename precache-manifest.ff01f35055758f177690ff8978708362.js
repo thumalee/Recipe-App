2c2d2c9104d804b80bdba02ce5c5dbeef37e253d
ASUS <thumalee@outlook.com>
2020-05-11 23:05:18 +0530

    Updates

---
+++ precache-manifest.ff01f35055758f177690ff8978708362.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60883f3673ae9eb62f7c77bf315a44a0",
+    "url": "/Recipe-App/index.html"
+  },
+  {
+    "revision": "d0fb25f486502b4a5782",
+    "url": "/Recipe-App/static/css/main.9aa2036f.chunk.css"
+  },
+  {
+    "revision": "00f408812ce82bf26fe8",
+    "url": "/Recipe-App/static/js/2.5318ca6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe-App/static/js/2.5318ca6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0fb25f486502b4a5782",
+    "url": "/Recipe-App/static/js/main.62e334e2.chunk.js"
+  },
+  {
+    "revision": "67567edc6e80ae196999",
+    "url": "/Recipe-App/static/js/runtime-main.9c5f599c.js"
+  }
+]);
